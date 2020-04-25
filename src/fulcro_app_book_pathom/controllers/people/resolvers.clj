a3e490fcca0b0c5e788e956659e2dc1dd5d9df3a
Rafael Delboni <rafadelboni@gmail.com>
2020-04-25 11:18:21 -0300

    fix: Adds docstring

--- src/fulcro_app_book_pathom/controllers/people/resolvers.clj
+++ src/fulcro_app_book_pathom/controllers/people/resolvers.clj
@@ -3,6 +3,7 @@
     [com.wsscode.pathom.connect :as pc]))
 
 (def people-table
+  "list with people"
   (atom
     {1 {:person/id 1 :person/name "Sally" :person/age 32}
      2 {:person/id 2 :person/name "Joe" :person/age 22}
@@ -10,6 +11,7 @@
      4 {:person/id 4 :person/name "Bobby" :person/age 55}}))
 
 (def list-table
+  "list with all friends and enemies relationship"
   (atom
     {:friends {:list/id     :friends
                :list/label  "Friends"
@@ -18,34 +20,34 @@
                :list/label  "Enemies"
                :list/people [4 3]}}))
 
-;; Given :person/id, this can generate the details of a person
-(pc/defresolver person-resolver [env {:person/keys [id]}]
+(pc/defresolver person-resolver 
+  "Given :person/id, this can generate the details of a person"
+  [env {:person/keys [id]}]
   {::pc/input  #{:person/id}
    ::pc/output [:person/name :person/age]}
   (get @people-table id))
 
-;; Given a :list/id, this can generate a list label and the people
-;; in that list (but just with their IDs)
-(pc/defresolver list-resolver [env {:list/keys [id]}]
+(pc/defresolver list-resolver
+  "Given a :list/id, this can generate a list label and the people
+  in that list (but just with their IDs)"
+  [env {:list/keys [id]}]
   {::pc/input  #{:list/id}
    ::pc/output [:list/label {:list/people [:person/id]}]}
   (when-let [list (get @list-table id)]
     (assoc list
            :list/people (mapv (fn [id] {:person/id id}) (:list/people list)))))
 
-(pc/defresolver friends-resolver [env input]
+(pc/defresolver friends-resolver
+  "Given the output {:friends [:list/id]} without input resolve list of friends"
+  [env input]
   {::pc/output [{:friends [:list/id]}]}
   {:friends {:list/id :friends}})
 
-(pc/defresolver enemies-resolver [env input]
+(pc/defresolver enemies-resolver
+  "Given the output {:enemies [:list/id]} without input resolve list of enemies"
+  [env input]
   {::pc/output [{:enemies [:list/id]}]}
   {:enemies {:list/id :enemies}})
-
-
-; Given a :input/id and :input/name return :input/result
-;(fulcro-app-book-pathom.pathom.parser/pathom-parser
-  ;{}
-  ;'[{([:input/id 123] {:pathom/context {:input/name 123}}) [:input/result]}])
 
 (pc/defresolver test-resolver
   [env {:input/keys [id name]}]
@@ -53,5 +55,10 @@
                  :input/name}
    ::pc/output [:input/result]}
   {:input/result (str id "-" name)})
+
+; Given a :input/id and :input/name return :input/result
+;(fulcro-app-book-pathom.pathom.parser/pathom-parser
+  ;{}
+  ;'[{([:input/id 123] {:pathom/context {:input/name 123}}) [:input/result]}])
 
 (def resolvers [person-resolver list-resolver friends-resolver enemies-resolver test-resolver])
